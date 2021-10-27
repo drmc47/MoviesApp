901eeda89a2be5a1f3f2d144b01bb3f67722f4d5
Daniel Mojica <drmc47@gmail.com>
2021-10-27 18:16:02 -0300

    fixed favs

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,15 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { addFavourite } from "../../actions";
 import "./Card.css";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import styled from 'styled-components'
+
+
 function Card({ name, image, id }) {
   const dispatch = useDispatch();
   let addFav = () => {
     dispatch(addFavourite({name, image, id}))
+    alert('ADDED!')
   }
   return (
     <div className=" d-flex m-3">
@@ -20,9 +25,13 @@ function Card({ name, image, id }) {
       >
         <h2>{name}</h2>
       </Link>
-        <button onClick={addFav}>❤️</button>
+        <Like onClick={() => addFav()}/>
     </div>
   );
 }
+
+const Like = styled(FavoriteIcon)`
+  font-size: 2rem;
+`
 
 export default Card;
